cae4e3cd36d56b43f4b8bec46a48d6af252d72c6
Hung Lee <56537311+charlieanston@users.noreply.github.com>
2023-06-08 20:11:11 +0700

    header
    header

--- app/frontend/src/components/header/Header.jsx
+++ app/frontend/src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import {Buttons} from '../../components'
+import { ProfilePic} from '../../asset/images/index.js';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex flex-row'>
+      <div className='flex basis-1/2 justify-start flex-row'>
+        <div className='w-14 h-14 bg-primary rounded-lg'>
+          <ProfilePic className='w-14 h-14'/>
+        </div>
+        <div className='flex flex-col'>
+          <p>text</p>
+          <p>text</p>
+        </div>
+      </div>
+      <div className='flex basis-1/2 justify-end'>
+        <Buttons context="cart"></Buttons>
+      </div>
+    </div>
   )
 }
 

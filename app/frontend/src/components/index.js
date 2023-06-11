2f4fb38b555e3b42e674bb86a007e9e5214a41a8
KevinDang38 <56989441+KevinDang38@users.noreply.github.com>
2023-06-11 23:40:13 +0700

    Update index.js

--- app/frontend/src/components/index.js
+++ app/frontend/src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Boxes } from './boxes/Boxes';
 export { default as Buttons } from './buttons/Buttons';
+export { default as Categories } from './categories/Categories';
 export { default as Header } from './header/Header';
 export { default as Navigation } from './navigation/Navigation';
 export { default as Popups } from './popups/Popups';

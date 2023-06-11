9c62fd738f3bce65c7c0c042db11c1db4ad6dc70
KevinDang38 <56989441+KevinDang38@users.noreply.github.com>
2023-06-11 23:57:27 +0700

    Update Popups.jsx

--- app/frontend/src/components/popups/Popups.jsx
+++ app/frontend/src/components/popups/Popups.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import Buttons from '../buttons/Buttons';
 import './popup.css'
 
-const Popups = ({visible}) => {
-  // if (!visible) return null;
+const Popups = ({visible, title, description, context}) => {
+  if (!visible) return null; //Remove the ! for the popup to appear
 
   return (
-    <div className='fixed inset-x-0 bottom-0 bg-black bg-opacity-30 flex'>
-      <div className='bg-white p-2 rounded-t-lg h-fit w-screen text-center'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ut magni rerum atque vel quo fugiat eveniet praesentium tempora, magnam quidem maxime quaerat aliquid perferendis quisquam repellendus. Eligendi eaque recusandae, error distinctio aperiam ad veniam quam aspernatur! Atque, cumque similique! Vero quisquam saepe quasi veniam facere ea id culpa mollitia corporis consectetur? Expedita laudantium quisquam voluptates accusamus tempore cumque, ipsa nemo illum ullam distinctio id ipsam reiciendis suscipit repellendus eius nihil. Eum accusantium delectus quis tempora aliquam magnam dignissimos quisquam mollitia facilis libero a quam voluptatibus quaerat, molestiae eius reiciendis, voluptatum, nam officiis praesentium maxime voluptas. Qui omnis autem, perspiciatis est obcaecati voluptas accusantium praesentium eveniet sapiente veritatis, tenetur aperiam atque corrupti ullam dolore? Ipsam, dolorum eaque?
+    <div className='fixed inset-0 bg-black bg-opacity-50 flex items-end justify-center z-10'>
+      <div className='bg-white px-6 py-4 rounded-t-3xl w-full text-center'>
+        <h2 className='text-2xl font-bold mb-4'>This is the title</h2>
+        <p className='text-lg mb-3'>By clicking ‘Apply’ you agree to delete this item from your cart.</p>
+        <Buttons context='apply'></Buttons>
       </div>
     </div>
   );

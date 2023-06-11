eb52a8defb65bf5de95085e77af10e45382f2b1b
KevinDang38 <56989441+KevinDang38@users.noreply.github.com>
2023-06-11 10:44:59 +0700

    Organize the SVG files + Include Category box component

---
+++ app/frontend/src/asset/icons/navigation/index.js
@@ -0,0 +1,10 @@
+export { ReactComponent as HomeNav } from './home_nav.svg';
+export { ReactComponent as CartNav } from './cart_nav.svg';
+export { ReactComponent as ScanQRNav } from './qr_nav.svg';
+export { ReactComponent as OnGoingNav } from './on_going_nav.svg';
+export { ReactComponent as ProfileNav } from './profile_nav.svg';
+export { ReactComponent as HomeNavActive } from './home_nav_active.svg';
+export { ReactComponent as CartNavActive } from './cart_nav_active.svg';
+export { ReactComponent as ScanQRNavActive } from './qr_nav_active.svg';
+export { ReactComponent as OnGoingNavActive } from './on_going_nav_active.svg';
+export { ReactComponent as ProfileNavActive } from './profile_nav_active.svg';

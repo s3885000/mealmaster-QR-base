394c4e580590a4ba54a4c7aef07233ffbf2d9f0e
HunkCao457 <s3756448@rmit.edu.vn>
2023-06-12 14:16:38 +0700

    add item entity

---
+++ app/backend/src/items/items.controller.ts
@@ -0,0 +1,48 @@
+import { Controller, Get, Post, Body, Put, Param, Delete, NotFoundException } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './entity/item.entity';
+
+@Controller('item')
+export class ItemsController {
+    constructor(private readonly itemsService: ItemsService) {}
+
+    //Get all items
+    @Get()
+    async findAll(): Promise<Item[]> {
+        return this.itemsService.findAll();
+    }
+
+    //Get item by id
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<Item> {
+        const item = await this.itemsService.findOne(id);
+        if (!item) {
+            throw new NotFoundException('Item not found!');
+        } else {
+            return item;
+        }
+    }
+
+    //Create item
+    @Post()
+    async create(@Body() item: Item): Promise<Item> {
+        return this.itemsService.create(item);
+    }
+
+    //Update item
+    @Put(':id')
+    async update (@Param('id') id: number, @Body() item: Item): Promise<any> {
+        return this.itemsService.update(id, item);
+    }
+
+    //Delete item
+    @Delete(':id')
+    async delete(@Param('id') id: number): Promise<any> {
+        //Handle the error if item does not exist
+        const item = await this.itemsService.findOne(id);
+        if (!item) {
+            throw new NotFoundException('Item does not exist!');
+        }
+        return this.itemsService.delete(id);
+    }
+}

234f1e6efd66ebb68f2dea599be47d3dc0cc26b2
HunkCao457 <s3756448@rmit.edu.vn>
2023-06-13 12:35:41 +0700

    add category

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   dotenv.config({ path: '.env' });
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3080);
 
 }
 bootstrap();

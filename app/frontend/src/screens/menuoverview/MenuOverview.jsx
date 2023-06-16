2d095067abb39328e65c8dc0ec7cd76ea12e769f
KevinDang38 <56989441+KevinDang38@users.noreply.github.com>
2023-06-16 16:59:10 +0700

    Update MenuOverview.jsx

--- app/frontend/src/screens/menuoverview/MenuOverview.jsx
+++ app/frontend/src/screens/menuoverview/MenuOverview.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Buttons, Information } from '../../components';
+import { Categories, Information, Navigation, Items } from '../../components';
 import './menuoverview.css';
 
 const MenuOverview = () => {
   return (
-    <div className="menu-overview">
-      <Information type="table_number" />
+    <div className='MenuOverview flex flex-col space-y-5 pb-20'>
       <Information type="address" />
+      <Categories />
+      <Items type='food_item'></Items>
+      <Items type='food_item'></Items>
+      <Items type='food_item'></Items>
+      <Items type='food_item'></Items>
+      <Items type='food_item'></Items>
+      <Items type='food_item'></Items>
+      <Items type='food_item'></Items>
+      <Items type='food_item'></Items>
+      <Items type='food_item'></Items>
+      <Items type='food_item'></Items>
+      <Navigation />
     </div>
   );
 };

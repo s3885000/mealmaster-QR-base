a91a1ccbd89e51558c5e001538972bd5b11cc188
Andiie <andiie@Andiies-MacBook-Air.local>
2023-07-11 13:44:32 +0700

    add AuthGuard & RolesGuard

--- app/backend/src/auth/guards/role.dectorator.ts
+++ app/backend/src/auth/guards/role.dectorator.ts
@@ -1,4 +1,4 @@
-import { SetMetadata } from "@nestjs/common";
-import { UserRole } from "src/user/entity/user.entity";
+import { SetMetadata } from '@nestjs/common';
+import { UserRole } from 'src/user/entity/user.entity';
 
 export const Roles = (...roles: UserRole[]) => SetMetadata('roles', roles);

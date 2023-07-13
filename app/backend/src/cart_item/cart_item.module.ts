61720ba4f62953a7272a2a3004ded3800dd2017d
HunkCao457 <s3756448@rmit.edu.vn>
2023-07-13 13:25:15 +0700

    update cart-item

--- app/backend/src/cart_item/cart_item.module.ts
+++ app/backend/src/cart_item/cart_item.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CartItemController } from './cart_item.controller';
+import { CartItemService } from './cart_item.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CartItem } from './entity/cartItem.entity';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([CartItem])],
+    controllers: [CartItemController],
+    providers: [CartItemService]
+})
 export class CartItemModule {}

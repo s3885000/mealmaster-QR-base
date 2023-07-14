3ae9ce586a9f052ab18a49ab054ab4b24587f110
Hung Lee <56537311+charlieanston@users.noreply.github.com>
2023-07-14 16:07:23 +0700

    on-going line
    on-going line

--- app/frontend/src/components/progress/Progress.jsx
+++ app/frontend/src/components/progress/Progress.jsx
@@ -8,18 +8,18 @@ const Step = ({ currentStep, stepIndex }) => (
     <div className={`step w-10 h-10 rounded-md ${currentStep = stepIndex ? 'bg-primary' : 'bg-gray'} flex items-center justify-center`}>
       {currentStep = stepIndex ? <CheckIcon className='text-white' /> : <InProgressIcon className='text-white' />}
     </div>
-    <p className="text-sm text-center overflow-hidden truncate whitespace-nowrap">{steps[stepIndex]}</p>
+    {/* <p className="text-sm text-center overflow-hidden truncate whitespace-nowrap">{steps[stepIndex]}</p> */}
   </div>
 )
 
 const Progress = ({ currentStep }) => (
-  <div className="h-full flex justify-center items-center pr-8">
+  <div className="h-full flex justify-center items-center pr-5">
     {steps.map((step, index) => (
       <Step currentStep={currentStep} stepIndex={index} key={step} />
     ))}
-    <div className='flex flex-row absolute pl-5'>
-      <div className='bg-gray w-28 h-1 rounded-full hover:bg-primary mr-3'/>
-      <div className='bg-gray w-28 h-1 rounded-full hover:bg-primary ml-3'/>
+    <div className='flex flex-row absolute'>
+      <div className='bg-gray w-20 h-1 hover:bg-primary mr-5 inset-0'/>
+      <div className='bg-gray w-20 h-1 hover:bg-primary ml-5'/>
     </div>
   </div>
 );

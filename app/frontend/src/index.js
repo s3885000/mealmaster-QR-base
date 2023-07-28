a0c8c1a98ef6274a45f760dd763e611452b4749a
Andiie <s3885000@rmit.edu.vn>
2023-07-29 00:59:16 +0700

    downgrade react 17

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -6,14 +6,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 reportWebVitals();

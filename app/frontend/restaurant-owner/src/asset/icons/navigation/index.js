4aa6a9ebfdb7a4f8350b90d6a0fcd0185a2ea8a2
KevinDang38 <56989441+KevinDang38@users.noreply.github.com>
2023-08-09 11:32:43 +0700

    Navigation for CMS

---
+++ app/frontend/restaurant-owner/src/asset/icons/navigation/index.js
@@ -0,0 +1,10 @@
+export { ReactComponent as HomeNav } from './cms_home_nav.svg';
+export { ReactComponent as TableNav } from './cms_table_nav.svg';
+export { ReactComponent as MenuNav } from './cms_menu_nav.svg';
+export { ReactComponent as OnGoingNav } from './cms_ongoing_nav.svg';
+export { ReactComponent as HistoryNav } from './cms_history_nav.svg';
+export { ReactComponent as HomeNavActive } from './cms_home_nav_active.svg';
+export { ReactComponent as TableNavActive } from './cms_table_nav_active.svg';
+export { ReactComponent as MenuNavActive } from './cms_menu_nav_active.svg';
+export { ReactComponent as OnGoingNavActive } from './cms_ongoing_nav_active.svg';
+export { ReactComponent as HistoryNavActive } from './cms_history_nav_active.svg';

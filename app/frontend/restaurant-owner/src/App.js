f2909bd1b31d8b8c723e4df09866731d95d51d83
IuKim <s3805504@rmit.edu.vn>
2023-08-14 20:29:18 +0700

    stuff

--- app/frontend/restaurant-owner/src/App.js
+++ app/frontend/restaurant-owner/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigation } from './components'
+import { Navigation, Popups } from './components'
 
 const App = () => {
   return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/navigation" element={<Navigation />} /> 
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Navigation />
+    //   <Routes>
+    //     <Route path="/navigation" element={<Navigation />} /> 
+    //   </Routes>
+    // </Router>
+    <Popups />
   );
 };
 
--- app/frontend/restaurant-owner/src/components/popups/Popups.jsx
+++ app/frontend/restaurant-owner/src/components/popups/Popups.jsx
@@ -28,6 +28,17 @@ const Popups = ({visible, type, onClose}) => {
     };
   }, [popupRef]);
 
+  const popupHeader = {
+    'order_completed': (
+      <>
+        <h2 className='text-2xl font-bold mb-4'>Order Completed!</h2>
+        <p className='text-lg mb-3'>Register to save your order history</p>
+        <Buttons context='rate' className='mb-5' onClick={() => handleNavigation('/on-going')}></Buttons>
+        <Buttons context='order_smth' onClick={() => handleNavigation('/menu-overview')}></Buttons>
+      </>
+    ),
+  };
+
   const popupContent = {
     'order_completed': (
       <>
@@ -37,55 +48,19 @@ const Popups = ({visible, type, onClose}) => {
         <Buttons context='order_smth' onClick={() => handleNavigation('/menu-overview')}></Buttons>
       </>
     ),
-    'thank_you': (
-      <>
-        <h2 className='text-2xl font-bold mb-4'>Thank you for your order</h2>
-        <p className='text-lg mb-3'>Track your order in the “On Going” screen</p>
-        <Buttons context='on_going' className='mb-5' onClick={() => handleNavigation('/on-going')}></Buttons>
-      </>
-    ),
-    'notes': (
-      <>
-        <h2 className='text-2xl font-bold mb-4'>Edit notes</h2>
-        <div className='mb-3.75'>
-          <textarea
-            className='px-2 text-l bg-gray opacity-50 text-black font-medium rounded-lg mb-5 w-full h-24 resize-none'
-            name='foodItemNotes'
-            placeholder='Enter your notes here'
-          />
-        </div>
-        <Buttons context='apply' className='mb-5' />
-      </>
-    ),
-    'cart_empty': (
-      <>
-        <h2 className='text-2xl font-bold mb-4'>Cart is Empty</h2>
-        <p className='text-lg mb-3'>Your cart is currently empty, please add more item to view your cart.</p>
-        <Buttons context='add_more' className='mb-5' />
-      </>
-    ),
-    'delete_item': (
-      <>
-        <h2 className='text-2xl font-bold mb-4'>Delete this Item</h2>
-        <p className='text-lg mb-3'>By clicking ‘Apply’ you agree to delete this item from your cart.</p>
-        <Buttons context='apply' className='mb-5' />
-      </>
-    ),
-    'food_ready': (
-      <>
-        <h2 className='text-2xl font-bold mb-4'>Food is Ready</h2>
-        <p className='text-lg mb-3'>Your food is ready to pick up at counter! Press Order Received when you’re done.</p>
-        <Buttons context='close' className='mb-5' onClick={onClose}></Buttons>
-      </>
-    ),
   };
 
   if (!showPopup) return null;
 
   return (
     <div className='fixed inset-0 bg-black bg-opacity-50 z-50 h-screen'>
-      <div ref={popupRef} className={`absolute bottom-0 bg-white px-6 py-4 rounded-t-3xl w-full text-center animate-slide-up ${showPopup ? 'show' : ''}`}>
-        {popupContent[type]}
+      <div className={`animate-slide-up ${showPopup ? 'show' : ''}`}>
+        <div ref={popupRef} className={`bg-gray px-6 py-4 rounded-t-3xl w-full text-center`}>
+          {popupHeader[type]}
+        </div>
+        <div ref={popupRef} className={`bg-white px-6 py-4 rounded-b-3xl w-full text-center`}>
+          {popupContent[type]}
+        </div>
       </div>
     </div>
   );

1b9b3a2ad3cd0152519f2381fcab3cab8bc26b69
KevinDang38 <56989441+KevinDang38@users.noreply.github.com>
2023-08-15 23:21:18 +0700

    update Navigation.jsx component

--- app/frontend/restaurant-owner/src/App.js
+++ app/frontend/restaurant-owner/src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigation, Header } from './components'
+import { Navigation, Header, Items } from './components'
 import './App.css';
-
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Navigation />
-      <Routes>
-        <Route path="/navigation" element={<Navigation />} /> 
-      </Routes>
+      <div className="main-content">
+        <Items type="tables" />
+        <Items type="categories" state="active" />
+        <Items type="categories" state="inactive" />
+        <Items type="food_item" state="active" />
+        <Items type="food_item" state="inactive" />
+        <Items type="orders" state="in_progress" />
+        <Items type="orders" state="active" />
+        <Items type="orders" state="inactive" />
+        <Routes>
+          <Route path="/navigation" element={<Navigation />} /> 
+        </Routes>
+      </div>
     </Router>
   );
 };

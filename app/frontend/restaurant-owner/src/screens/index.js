722cdee295e0c9ebd1119a7c856260aa530adcc1
HunkCao457 <s3756448@rmit.edu.vn>
2023-08-16 11:29:01 +0700

    .

--- app/frontend/restaurant-owner/src/screens/index.js
+++
@@ -1,9 +0,0 @@
-export { default as OnGoing } from './on-going/OnGoing';
-export { default as Dashboard } from './dashboard/Dashboard';
-export { default as History } from './history/History';
-export { default as Login } from './login/Login';
-export { default as FoodCategories } from './food-categories/FoodCategories';
-export { default as Profile } from './profile/Profile';
-export { default as Tables } from './tables/Tables';
-export { default as SignUp } from './sign-up/SignUp';
-

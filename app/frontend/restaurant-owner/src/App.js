fd6e7220e2a9b450e0fb6cdcb13ba4d68f95fa42
KevinDang38 <56989441+KevinDang38@users.noreply.github.com>
2023-08-17 23:42:16 +0700

    Popups.jsx

--- app/frontend/restaurant-owner/src/App.js
+++ app/frontend/restaurant-owner/src/App.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigation, Header, Items } from './components'
+import { Navigation, Header, Items, Buttons, Popups } from './components'; // Import Popups
 import './App.css';
 
 const App = () => {
+  const [activePopup, setActivePopup] = useState(null); // State to keep track of the active popup
+
+  // Show the specified popup
+  const showPopup = (popupType) => {
+    setActivePopup(popupType);
+  };
+
+  // Close the currently active popup
+  const closePopup = () => {
+    setActivePopup(null);
+  };
+
   return (
     <Router>
       <Header />
       <Navigation />
       <div className="main-content flex justify-center items-center min-h-screen">
         <div className="items-container space-y-2.5">
-          <Items type="tables" />
-          <Items type="categories" state="active" />
-          <Items type="categories" state="inactive" />
-          <Items type="food_item" state="active" />
-          <Items type="food_item" state="inactive" />
-          <Items type="orders" state="in_progress" />
-          <Items type="orders" state="active" />
-          <Items type="orders" state="inactive" />
+          <Buttons context='update' onClick={() => showPopup('alert')} />
+          <Buttons context='update' onClick={() => showPopup('add_table')} />
+          <Buttons context='update' onClick={() => showPopup('edit_food')} />
+          <Buttons context='update' onClick={() => showPopup('add_food')} />
         </div>
         <Routes>
           <Route path="/navigation" element={<Navigation />} /> 
         </Routes>
       </div>
+      {activePopup && <Popups type={activePopup} onClose={closePopup} visible={true} />}
     </Router>
   );
 };
 
 export default App;
-

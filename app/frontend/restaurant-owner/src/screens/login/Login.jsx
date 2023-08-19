ed623862d573b279162e42b485596c97f50dddf1
KevinDang38 <56989441+KevinDang38@users.noreply.github.com>
2023-08-19 21:46:19 +0700

    Optimize Login/SignUp/PasswordRecovery Screen

--- app/frontend/restaurant-owner/src/screens/login/Login.jsx
+++ app/frontend/restaurant-owner/src/screens/login/Login.jsx
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
 import { Buttons } from '../../components';  
+import { useNavigate } from 'react-router-dom';
 import { MealMasterLogo } from '../../asset/images/mealmaster_logo/index.js';
 
 const Login = () => {
+    const navigate = useNavigate();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(true); 
     const [emailError, setEmailError] = useState(null);
     const [passwordError, setPasswordError] = useState(null);
 
-    const handleContinueClick = () => {
+    const handleLogin = () => {
         // Implement the login functionality
     };
 
-    const handleGuestClick = () => {
-        // Implement the create account functionality
-    };
+    const redirectToSignup = () => {
+        navigate('/signup');
+    }
 
-    const handleForgotPasswordClick = () => {
-        // Implement the forgot password functionality
+    const redirectToPasswordRecovery = () => {
+        navigate('/password-recovery');
     };
 
     const togglePasswordVisibility = () => {
@@ -26,59 +29,59 @@ const Login = () => {
     };
 
     return (
-        <div className="flex h-screen">
-            {/* Left half (hidden on mobile screens) */}
-            <div className="hidden md:flex flex-col items-center justify-center flex-1 bg-gray-300 rounded-r-3xl">
+        <main className="flex h-screen">
+            {/* Left half */}
+            <section className="hidden md:flex flex-col items-center justify-center flex-1 bg-gray-300 rounded-r-3xl">
                 <MealMasterLogo className='w-50 h-50 rounded-xl'/> 
                 <h2 className="text-2xl font-bold mb-5">MealMaster</h2>
                 <p className="text-center text-md font-medium">Elevate customer experience with a QR code system</p>
-            </div>
+            </section>
             
             {/* Right half */}
-            <div className="flex flex-col items-center justify-start flex-1 bg-white p-10 w-full md:w-1/2">
-                <div className="mt-1/3 mb-4">
-                    <h1 className="flex flex-col items-center text-2xl font-bold mt-40">Welcome Back!</h1>
-                    <p className="flex flex-col items-center text-md font-medium">Log in to manage your restaurant</p>
-                </div>
+            <section className="flex flex-col items-center justify-start flex-1 bg-white p-10 w-full md:w-1/2">
+                <h1 className="text-2xl font-bold mt-40 mb-4">Welcome Back!</h1>
+                <p className="text-md font-medium mb-4">Log in to manage your restaurant</p>
 
-                <label className="block w-full mb-5">
-                    Email
-                    <input 
-                        id='emailInput'
-                        className='w-full h-10 px-7 text-sm bg-tertiary text-placeholders font-medium rounded-2xl mt-2'
-                        type='text'
-                        placeholder='Email Address'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </label>
-                {emailError && <div className='text-error text-xs mt-1'>{emailError}</div>}
-
-                <label className="block w-full mb-5">
-                    Password
-                    <div className="relative">
+                <form className="w-full">
+                    <label className="block w-full mb-5" htmlFor="emailInput">
+                        Email
                         <input 
-                            id='passwordInput'
+                            id='emailInput'
                             className='w-full h-10 px-7 text-sm bg-tertiary text-placeholders font-medium rounded-2xl mt-2'
-                            type={showPassword ? 'password' : 'text'}
-                            placeholder='Password'
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            type='email' // Use email type for email input
+                            placeholder='Email Address'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            aria-label="Email Address for Login"
                         />
-                        <button onClick={togglePasswordVisibility} className="absolute right-7 top-1/2 transform -translate-y-1/2">
-                            {showPassword ? "Show" : "Hide"}
-                        </button>
-                    </div>
-                </label>
-                {passwordError && <div className='text-error text-xs mt-1'>{passwordError}</div>}
+                    </label>
+                    {emailError && <div className='text-error text-xs mt-1'>{emailError}</div>}
 
-                <button onClick={handleForgotPasswordClick} className="text-gray mb-5 w-full text-left self-start">
-                    Forgot Password?
-                </button>
-                <Buttons context="login" className="mb-5" onClick={handleContinueClick}></Buttons>
-                <Buttons context="create_account_login" className="mb-5" onClick={handleGuestClick}></Buttons>
-            </div>
-        </div>
+                    <label className="block w-full mb-5" htmlFor="passwordInput">
+                        Password
+                        <div className="relative">
+                            <input 
+                                id='passwordInput'
+                                className='w-full h-10 px-7 text-sm bg-tertiary text-placeholders font-medium rounded-2xl'
+                                type={showPassword ? 'password' : 'text'}
+                                placeholder='Password'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                aria-label="Password for Login"
+                            />
+                            <Buttons context={showPassword ? "hide" : "view"} onClick={togglePasswordVisibility} className="absolute right-7 top-1/2 transform -translate-y-1/2" />
+                        </div>
+                    </label>
+                    {passwordError && <div className='text-error text-xs mt-1'>{passwordError}</div>}
+
+                    <button onClick={redirectToPasswordRecovery} className="text-gray mb-5 w-full text-left self-start">
+                        Forgot Password?
+                    </button>
+                    <Buttons context="login" className="mb-5" onClick={handleLogin}></Buttons>
+                    <Buttons context="create_account_login" className="mb-5" onClick={redirectToSignup}></Buttons>
+                </form>
+            </section>
+        </main>
     );
 };
 
--- app/frontend/restaurant-owner/src/screens/sign-up/SignUp.jsx
+++ app/frontend/restaurant-owner/src/screens/sign-up/SignUp.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { Buttons } from '../../components';  
+import { useNavigate } from 'react-router-dom';
 import { MealMasterLogo } from '../../asset/images/mealmaster_logo/index.js';
 
-const Login = () => {
+const Signup = () => {
+    const navigate = useNavigate();
+
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -11,16 +14,12 @@ const Login = () => {
     const [emailError, setEmailError] = useState(null);
     const [passwordError, setPasswordError] = useState(null);
 
-    const handleContinueClick = () => {
-        // Implement the login functionality
+    const handleCreateAccountClick = () => {
+        // Implement the signup functionality
     };
 
-    const handleGuestClick = () => {
-        // Implement the create account functionality
-    };
-
-    const handleForgotPasswordClick = () => {
-        // Implement the forgot password functionality
+    const handleLoginClick = () => {
+        navigate('/login');
     };
 
     const togglePasswordVisibility = () => {
@@ -28,69 +27,71 @@ const Login = () => {
     };
 
     return (
-        <div className="flex h-screen">
-            {/* Left half (hidden on mobile screens) */}
-            <div className="hidden md:flex flex-col items-center justify-center flex-1 bg-gray-300 rounded-r-3xl">
+        <main className="flex h-screen">
+            {/* Left half */}
+            <section className="hidden md:flex flex-col items-center justify-center flex-1 bg-gray-300 rounded-r-3xl">
                 <MealMasterLogo className='w-50 h-50 rounded-xl'/> 
                 <h2 className="text-2xl font-bold mb-5">MealMaster</h2>
                 <p className="text-center text-md font-medium">Elevate customer experience with a QR code system</p>
-            </div>
+            </section>
             
             {/* Right half */}
-            <div className="flex flex-col items-center justify-start flex-1 bg-white p-10 w-full md:w-1/2">
-                <div className="mt-1/3 mb-4">
-                    <h1 className="flex flex-col items-center text-2xl font-bold mt-40">Create an Account</h1>
-                    <p className="flex flex-col items-center text-md font-medium">Create an account to manage your restaurant</p>
-                </div>
+            <section className="flex flex-col items-center justify-start flex-1 bg-white p-10 w-full md:w-1/2">
+                <h1 className="text-2xl font-bold mt-40 mb-4">Create an Account</h1>
+                <p className="text-md font-medium mb-4">Create an account to manage your restaurant</p>
 
-                <label className="block w-full mb-5">
-                    Name
-                    <input 
-                        id='nameInput'
-                        className='w-full h-10 px-7 text-sm bg-tertiary text-placeholders font-medium rounded-2xl mt-2'
-                        type='text'
-                        placeholder='Name'
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </label>
-
-                <label className="block w-full mb-5">
-                    Email
-                    <input 
-                        id='emailInput'
-                        className='w-full h-10 px-7 text-sm bg-tertiary text-placeholders font-medium rounded-2xl mt-2'
-                        type='text'
-                        placeholder='Email Address'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </label>
-                {emailError && <div className='text-error text-xs mt-1'>{emailError}</div>}
-
-                <label className="block w-full mb-5">
-                    Password
-                    <div className="relative">
+                <form className="w-full">
+                    <label className="block w-full mb-5" htmlFor="nameInput">
+                        Name
                         <input 
-                            id='passwordInput'
+                            id='nameInput'
                             className='w-full h-10 px-7 text-sm bg-tertiary text-placeholders font-medium rounded-2xl mt-2'
-                            type={showPassword ? 'password' : 'text'}
-                            placeholder='Password'
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            type='text'
+                            placeholder='Name'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            aria-label="Name for Signup"
                         />
-                        <button onClick={togglePasswordVisibility} className="absolute right-7 top-1/2 transform -translate-y-1/2">
-                            {showPassword ? "Show" : "Hide"}
-                        </button>
-                    </div>
-                </label>
-                {passwordError && <div className='text-error text-xs mt-1'>{passwordError}</div>}
+                    </label>
+                    {nameError && <div className='text-error text-xs mt-1'>{nameError}</div>}
 
-                <Buttons context="create_account_signup" className="mb-5" onClick={handleContinueClick}></Buttons>
-                <Buttons context="already" className="mb-5" onClick={handleGuestClick}></Buttons>
-            </div>
-        </div>
+                    <label className="block w-full mb-5" htmlFor="emailInput">
+                        Email
+                        <input 
+                            id='emailInput'
+                            className='w-full h-10 px-7 text-sm bg-tertiary text-placeholders font-medium rounded-2xl mt-2'
+                            type='email' // Change type to email for better validation
+                            placeholder='Email Address'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            aria-label="Email Address for Signup"
+                        />
+                    </label>
+                    {emailError && <div className='text-error text-xs mt-1'>{emailError}</div>}
+
+                    <label className="block w-full mb-5" htmlFor="passwordInput">
+                        Password
+                        <div className="relative">
+                            <input 
+                                id='passwordInput'
+                                className='w-full h-10 px-7 text-sm bg-tertiary text-placeholders font-medium rounded-2xl'
+                                type={showPassword ? 'password' : 'text'}
+                                placeholder='Password'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                aria-label="Password for Signup"
+                            />
+                            <Buttons context={showPassword ? "hide" : "view"} onClick={togglePasswordVisibility} className="absolute right-7 top-1/2 transform -translate-y-1/2" />
+                        </div>
+                    </label>
+                    {passwordError && <div className='text-error text-xs mt-1'>{passwordError}</div>}
+
+                    <Buttons context="create_account_signup" className="mb-5" onClick={handleCreateAccountClick}></Buttons>
+                    <Buttons context="already" className="mb-5" onClick={handleLoginClick}></Buttons>
+                </form>
+            </section>
+        </main>
     );
 };
 
-export default Login;
+export default Signup;

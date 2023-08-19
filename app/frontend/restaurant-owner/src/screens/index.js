3fe61db6bf6ab809695f636b25c220abd4b8a1e3
KevinDang38 <56989441+KevinDang38@users.noreply.github.com>
2023-08-19 22:09:20 +0700

    Update folder

--- app/frontend/restaurant-owner/src/screens/index.js
+++ app/frontend/restaurant-owner/src/screens/index.js
@@ -3,7 +3,7 @@ export { default as Dashboard } from './dashboard/Dashboard';
 export { default as History } from './history/History';
 export { default as Login } from './login/Login';
 export { default as PasswordRecovery } from './password-recovery/PasswordRecovery';
-export { default as FoodCategories } from './food-categories/FoodCategories';
+export { default as Menu } from './menu/Menu';
 export { default as Profile } from './profile/Profile';
 export { default as Tables } from './tables/Tables';
 export { default as SignUp } from './sign-up/SignUp';

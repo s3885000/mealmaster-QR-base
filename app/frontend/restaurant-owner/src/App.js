23c1d331f74f093b076027e46f3cc9c9997de2b5
HunkCao457 <s3756448@rmit.edu.vn>
2023-08-24 22:17:53 +0700

    update

--- app/frontend/restaurant-owner/src/App.js
+++ app/frontend/restaurant-owner/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navigation, Header, Items, Buttons, Popups } from './components'; // Import Popups
+import { Profile } from './screens';
 import './App.css';
-import Profile from './screens/profile/Profile';
 
 const App = () => {
   const [activePopup, setActivePopup] = useState(null); // State to keep track of the active popup
@@ -18,26 +18,26 @@ const App = () => {
   };
 
   return (
-    <Router>
-      <Header />
-      <Navigation />
-      <div className="main-content flex justify-center items-center min-h-screen">
-        <div className="items-container space-y-2.5">
-          <Buttons context='update' onClick={() => showPopup('alert')} />
-          <Buttons context='update' onClick={() => showPopup('add_table')} />
-          <Buttons context='update' onClick={() => showPopup('edit_food')} />
-          <Buttons context='update' onClick={() => showPopup('add_food')} />
-          <Buttons context='update' onClick={() => showPopup('order_details')} />
-          <Buttons context='update' onClick={() => showPopup('order_details_ready')} />
-          <Buttons context='update' onClick={() => showPopup('order_details_completed')} />
-        </div>
-        <Routes>
-          <Route path="/navigation" element={<Navigation />} /> 
-          <Route path="/profile" element={<Profile />} /> 
-        </Routes>
-      </div>
-      {activePopup && <Popups type={activePopup} onClose={closePopup} visible={true} />}
-    </Router>
+    <Profile />
+    // <Router>
+    //   <Header />
+    //   <Navigation />
+    //   <div className="main-content flex justify-center items-center min-h-screen">
+    //     <div className="items-container space-y-2.5">
+    //       <Buttons context='update' onClick={() => showPopup('alert')} />
+    //       <Buttons context='update' onClick={() => showPopup('add_table')} />
+    //       <Buttons context='update' onClick={() => showPopup('edit_food')} />
+    //       <Buttons context='update' onClick={() => showPopup('add_food')} />
+    //       <Buttons context='update' onClick={() => showPopup('order_details')} />
+    //       <Buttons context='update' onClick={() => showPopup('order_details_ready')} />
+    //       <Buttons context='update' onClick={() => showPopup('order_details_completed')} />
+    //     </div>
+    //     <Routes>
+    //       <Route path="/navigation" element={<Navigation />} /> 
+    //     </Routes>
+    //   </div>
+    //   {activePopup && <Popups type={activePopup} onClose={closePopup} visible={true} />}
+    // </Router>
   );
 };
 
--- app/frontend/restaurant-owner/src/components/popups/Popups.jsx
+++ app/frontend/restaurant-owner/src/components/popups/Popups.jsx
@@ -1,16 +1,38 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { Buttons } from '../../components';
+import { Upload } from '../../asset/icons/misc/index.js';
+// import { Form, Button } from 'semantic-ui-react';
 import { useForm } from "react-hook-form";
 
 const Popups = ({ visible, type, onClose }) => {
   const [showPopup, setShowPopup] = useState(false);
   const popupRef = useRef(null);
 
-  const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = (data) => {
-        console.log(data);
-    }
+  // For validation
+  // const [errors, setErrors] = useState({})
+  // const [addTableData, setAddTableData] = useState({
+  //   addTableName: ''
+  // })
+  // const handleAddTableChange = (e) => {
+  //   const {name, value} = e.target;
+  //   setAddTableData({
+  //     ...addTableData, [name] : value
+  //   })
+  // }
 
+  // const handleAddTableSubmit = (e) =>{
+  //   e.preventDefault()
+  //   const validationErrors = {}
+  //   if(!addTableData.addTableName.trim()) {
+  //     validationErrors.addTableName = "Table name is required"
+  //   }
+
+  //   setErrors(validationErrors)
+
+  //   if(Object.keys(validationErrors).length === 0) {
+  //     alert("Form submitted successfully")
+  //   }
+  // }
 
   const handleOnClose = () => {
     if (onClose) {
@@ -43,6 +65,11 @@ const Popups = ({ visible, type, onClose }) => {
     </div>
   );
 
+  const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
   const popupContent = {
     'alert': (
       <>
@@ -54,25 +81,26 @@ const Popups = ({ visible, type, onClose }) => {
         ]} />
       </>
     ),
-
     'add_table': (
       <>
         <PopupHeader title="Add Table" />
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label className="block text-2xl font-bold">Name</label>
-            <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter table name" type="text" name="addTableName"
-            {...register("addTableName", { required: true, maxLength: 10})}
-            />
-            {errors.addTableName && <p>Invalid</p>}
 
-            <PopupFooter buttons={[
+        <form onSubmit={handleSubmit(onSubmit)}>
+          
+            <label className="block text-2xl font-bold">Name</label>
+                <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter table name" type="text" name="addTableName"
+                {...register("addTableName", { required: true, maxLength: 10})}
+                />
+            {errors.addTableName && <p>Please check table name</p>}
+          
+            
+          <PopupFooter buttons={[
               { context: 'cancel', onClick: handleOnClose },
-              { context: 'add' }
-            ]} />
+              { context: 'add', }
+        ]} />  
         </form>
       </>
     ),
-
     'edit_table': (
       <>
         <PopupHeader title="Edit Table" />
@@ -81,16 +109,15 @@ const Popups = ({ visible, type, onClose }) => {
             <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter table name" type="text" name="editTableName" 
             {...register("editTableName", { required: true, maxLength: 10})}
             />
-            {errors.editTableName && <p>Invalid</p>}
-
-            <PopupFooter buttons={[
-              { context: 'cancel', onClick: handleOnClose },
-              { context: 'update' }
-            ]} />
+            {errors.addTableName && <p>Please check table name</p>}
+          <PopupFooter buttons={[
+            { context: 'cancel', onClick: handleOnClose },
+            { context: 'update' }
+          ]} />
         </form>
+        
       </>
     ),
-
     'add_category': (
       <>
         <PopupHeader title="Add Category" />
@@ -99,16 +126,15 @@ const Popups = ({ visible, type, onClose }) => {
             <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter table name" type="text" name="addCategoryName" 
             {...register("addCategoryName", { required: true, maxLength: 10})}
             />
-            {errors.addCategoryName && <p>Invalid</p>}
-
-            <PopupFooter buttons={[
-              { context: 'cancel', onClick: handleOnClose },
-              { context: 'add' }
-            ]} />
+            {errors.addTableName && <p>Please check table name</p>}
+        <PopupFooter buttons={[
+          { context: 'cancel', onClick: handleOnClose },
+          { context: 'add', onClick: handleOnClose }
+        ]} />
         </form>
+        
       </>
     ),
-
     'edit_category': (
       <>
         <PopupHeader title="Edit Category" />
@@ -117,17 +143,15 @@ const Popups = ({ visible, type, onClose }) => {
             <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter table name" type="text" name="editCategoryName" 
             {...register("editCategoryName", { required: true, maxLength: 10})}
             />
-            {errors.editCategoryName && <p>Invalid</p>}
-
-            <PopupFooter buttons={[
-              { context: 'cancel', onClick: handleOnClose },
-              { context: 'update' }
-            ]} />
+            {errors.addTableName && <p>Please check table name</p>}
+        <PopupFooter buttons={[
+          { context: 'cancel', onClick: handleOnClose },
+          { context: 'update', onClick: handleOnClose }
+        ]} />
         </form>
         
       </>
     ),
-
     'add_food': (
       <>
         <PopupHeader title="Add Food" />
@@ -136,73 +160,73 @@ const Popups = ({ visible, type, onClose }) => {
             <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter food name" type="text" name="foodName" 
             {...register("foodName", { required: true, maxLength: 10})}
             />
-            {errors.foodName && <p>Invalid</p>}
+            {errors.foodName && <p>Please check table name</p>}
 
             <label className='block text-2xl font-bold'>Description</label>
             <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter description" type="text" name="description" 
             {...register("description", { required: true, maxLength: 10})}
             />
-            {errors.description && <p>Invalid</p>}
+            {errors.description && <p>Please check table name</p>}
 
             <label className='block text-2xl font-bold'>Price (VND)</label>
             <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter price" type="text" name="price" 
             {...register("price", { required: true, maxLength: 10})}
             />
-            {errors.price && <p>Invalid</p>}
+            {errors.price && <p>Please check table name</p>}
 
-            <label class="block pt-4">
-              <input  type="file" name="table_name" accept='image/*' hidden/>
-              <div className='outline-dashed outline-2 outline-black rounded-md w-fit grid justify-items-center p-4'>
-                <p className='text-center'>Recommended Image Sizes: 400px x 300px<br/>Click to select a file from your device (png or jpeg)</p>
-              </div>
-            </label>
-
-            <PopupFooter buttons={[
-              { context: 'cancel', onClick: handleOnClose },
-              { context: 'add', }
-            ]} />
+          <label class="block pt-4">
+            <input  type="file" name="table_name" accept='image/*' hidden/>
+            <div className='outline outline-2 outline-black outline-dashed rounded-md w-fit grid justify-items-center p-4'>
+              <Upload className='w-14 h-14'/>
+              <p className='text-center'>Preferred size is 400px * 300px<br/>Drag 'n' drop some files here, or click to select file</p>
+            </div>
+          </label>
+          <PopupFooter buttons={[
+          { context: 'cancel', onClick: handleOnClose },
+          { context: 'add' }
+          ]} />
         </form>
+        
       </>
     ),
-
     'edit_food': (
       <>
         <PopupHeader title="Edit Food" />
         <form onSubmit={handleSubmit(onSubmit)}>
             <label className="block text-2xl font-bold">Name</label>
-            <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter food name" type="text" name="foodName" 
-            {...register("foodName", { required: true, maxLength: 10})}
+            <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter food name" type="text" name="food_name" 
+            {...register("food_name", { required: true, maxLength: 10})}
             />
-            {errors.foodName && <p>Invalid</p>}
+            {errors.price && <p>Please check table name</p>}
 
             <label className='block text-2xl font-bold'>Description</label>
             <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter description" type="text" name="description" 
             {...register("description", { required: true, maxLength: 10})}
             />
-            {errors.description && <p>Invalid</p>}
-            
+            {errors.price && <p>Please check table name</p>}
+
             <label className='block text-2xl font-bold'>Price (VND)</label>
             <input className="border border-gray rounded-md placeholder-slate-400 w-full p-2" placeholder="Enter price" type="text" name="price" 
             {...register("price", { required: true, maxLength: 10})}
             />
-            {errors.price && <p>Invalid</p>}
-            
-            <label class="block pt-4">
-              <input  type="file" name="table_name" accept='image/*' hidden/>
-              <div className='outline-dashed outline-2 outline-black rounded-md w-fit grid justify-items-center p-4'>
-                <p className='text-center'>Recommended Image Sizes: 400px x 300px<br/>Click to select a file from your device (png or jpeg)</p>
-              </div>
-            </label>
+            {errors.price && <p>Please check table name</p>}
 
-            <PopupFooter buttons={[
-              { context: 'cancel', onClick: handleOnClose },
-              { context: 'update', }
-            ]} />
+          <label class="block pt-4">
+            <input  type="file" name="table_name" accept='image/*' hidden/>
+            <div className='outline outline-2 outline-black outline-dashed rounded-md w-fit grid justify-items-center p-4'>
+              <Upload className='w-14 h-14'/>
+              <p className='text-center'>Preferred size is 400px * 300px<br/>Drag 'n' drop some files here, or click to select file</p>
+            </div>
+          </label>
+          <PopupFooter buttons={[
+            { context: 'cancel', onClick: handleOnClose },
+            { context: 'update' }
+          ]} />
         </form>
+        
       </>
     ),
-
-    'history': (
+    'order_details': (
       <>
         <PopupHeader title="Order Details" />
         <div className="divide-y divide-dashed">
@@ -252,109 +276,7 @@ const Popups = ({ visible, type, onClose }) => {
           </table>
           </div>
       </>
-    ),
-    'order_details_ready': (
-      <>
-        <PopupHeader title="Order Details" />
-        <div className="divide-y divide-dashed">
-          <table className='text-left border-separate border-spacing-x-4 border-spacing-y-2'>
-            <thead>
-              <tr>
-                <th>Order ID</th>
-                <th>Timestamp</th>
-                <th>Table</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data_1.map((val, key) => (
-                <tr key={key}>
-                  <td>{val.order_id}</td>
-                  <td>{val.timestamp}</td>
-                  <td>{val.table}</td>
-                  <td>{val.total}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <table className="text-left border-separate border-spacing-x-4 border-spacing-y-2">
-            <thead>
-              <tr>
-                <th>Quantity</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Notes</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data_2.map((val, key) => (
-                <tr key={key}>
-                  <td>{val.quantity}</td>
-                  <td>{val.name}</td>
-                  <td>{val.price}</td>
-                  <td>{val.notes}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <PopupFooter buttons={[
-          { context: 'ready', onClick: handleOnClose }
-        ]} />
-      </>
-    ),
-    'order_details_inprogress': (
-      <>
-        <PopupHeader title="Order Details" />
-        <div className="divide-y divide-dashed">
-          <table className='text-left border-separate border-spacing-x-4 border-spacing-y-2'>
-            <thead>
-              <tr>
-                <th>Order ID</th>
-                <th>Timestamp</th>
-                <th>Table</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data_1.map((val, key) => (
-                <tr key={key}>
-                  <td>{val.order_id}</td>
-                  <td>{val.timestamp}</td>
-                  <td>{val.table}</td>
-                  <td>{val.total}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <table className="text-left border-separate border-spacing-x-4 border-spacing-y-2">
-            <thead>
-              <tr>
-                <th>Quantity</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Notes</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data_2.map((val, key) => (
-                <tr key={key}>
-                  <td>{val.quantity}</td>
-                  <td>{val.name}</td>
-                  <td>{val.price}</td>
-                  <td>{val.notes}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <PopupFooter buttons={[
-          { context: 'decline', onClick: handleOnClose },
-          { context: 'accept', onClick: handleOnClose }
-        ]} />
-      </>
-    ),
-    
+    )    
   };
 
   useEffect(() => {
